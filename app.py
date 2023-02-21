a44cd1f0088b3d9a197d76727998952d785a8fe1
gus <19171@wc.school.nz>
2023-02-22 10:15:43 +1300

    DID DATABSE STUFF!

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
+
+DATABASE = "C:/Users/19171/PycharmProjects/PythonCafe/smile.db"
 
 app = Flask(__name__)
 
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Yo mama!'
+def render_homepage():  # put application's code here
+    return render_template('home.html')
 
+@app.route('/menu')
+def render_menu_page():
+    con = create_connection(DATABASE)
+    query = "SELECT name, description, volume, image, price FROM products"
+    cur = con.cursor()
+    cur.execute(query)
+    product_list = cur.fetchall()
+    con.close()
+    print(product_list)
+    return render_template('menu.html', products=product_list)
+
+@app.route('/contact')
+def render_contact_page():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
